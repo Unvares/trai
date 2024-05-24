a968f3753858f3c2832a7f441bcf8301322c9985
hs223nr <hs223nr@student.lnu.se>
2024-05-24 14:39:10 +0200

    chore(aboutus&contactus): AboutUs Text and stock image, ContactUs emails.

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -1,27 +1,53 @@
 <template>
-    <v-container class="about-section" id="about">
-      <img src="/assets/images/about.png" alt="About Us Image">
-      <div class="text">
-        <h2>About Us</h2>
-        <p>We are dedicated to promoting smart recycling habits.</p>
-      </div>
-    </v-container>
-  </template>
+  <v-container class="about-section" id="about">
+    <img src="/assets/images/about.png" alt="About Us Image">
+    <div class="text">
+      <h2>About Us</h2>
+      <p>Welcome to Trash Recycling AI (TRAI)! We're on a mission to 
+        revolutionize waste management and promote recycling through 
+        education and engagement.</p><br>
+      <p>The world faces a critical waste management challenge, with 
+        only a fraction of global waste being recycled. Our inspiration 
+        comes from the urgent need highlighted by the United Nations 
+        for enhanced waste management practices to combat pollution, 
+        climate change, and financial losses.</p><br>
+      <p>One of the key issues we're addressing is improper waste 
+        separation, a significant contributor to inefficient recycling. 
+        Studies reveal that lack of knowledge, accessibility to recycling 
+        facilities, and perceived inconvenience hinder proper waste 
+        sorting. Our goal is to make recycling education accessible, 
+        engaging, and effortless.</p><br>
+      <p>TRAI is a web-based application powered by AI chatbots, offering
+        rapid and interactive education on waste disposal. Through 
+        quizzes, playful gamification, and informative content, we aim 
+        to empower individuals with the knowledge and motivation to 
+        recycle correctly.</p><br>
+      <p>Unlike traditional solutions, TRAI minimizes deployment and 
+        maintenance costs while ensuring scalability. By leveraging 
+        custom GPTs, our platform can adapt to different waste separation 
+        rules and cater to diverse user needs.</p><br>
+      <p>Join us in our mission to create a world where recycling is 
+        second nature, and waste becomes a valuable resource. Together, 
+        let's build a cleaner, greener future with TRAI. </p>
+    </div>
+  </v-container>
+</template>
+
+
+<style scoped lang="scss">
+.about-section {
+  text-align: center;
+  margin: 20px 0;
+  background: #8B4513;
+  border-radius: 10px;
   
-  <style scoped lang="scss">
-  .about-section {
-    text-align: center;
-    margin: 20px 0;
-    background: #8B4513;
-    border-radius: 10px;
-    
-    img {
-      width: 200px;
-      height: 200px;
-    }
-    
-    .text {
-      margin-top: 20px;
-    }
+  img {
+    width: auto;
+    height: auto;
   }
+  
+  .text {
+    margin-top: 20px;
+  }
+}
 </style>
